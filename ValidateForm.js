08e2258f69f51474e0c9bff72faf4672bb83bf45
tringapps-Mariappan <123533770+Mariviknesh@users.noreply.github.com>
2023-03-10 11:45:26 +0530

    initial commit

---
+++ ValidateForm.js
@@ -0,0 +1,56 @@
+let check=()=>{
+    let name=document.getElementById("name");
+    let number=document.getElementById("number");
+    let date=document.getElementById("date");
+    let num=number.value.toString();
+    let nameerr=document.getElementById("nameerr");
+    let numerr=document.getElementById("numerr");
+    let dateerr=document.getElementById("dateerr");
+    var letters = /^[A-Za-z]+$/;
+    let nameresult=true;
+    let numresult=true;
+    let dateresult=true;
+    if(name.value==""||!name.value.match(letters))
+    {
+        document.getElementById("nameerr").innerHTML="Invalid input text";
+        console.log("name error");
+        console.log(letters);
+        nameresult=false
+    }
+    else{
+        document.getElementById("nameerr").innerHTML="-";
+        nameresult=true;
+        nameerr.style.color="white";
+    }
+    if(num.length!=10||num[0]==0||number.value=="")
+    {
+        document.getElementById("numerr").innerHTML="Invalid phone number";
+        console.log("number error");
+        numresult=false;
+    }
+    else{
+        document.getElementById("numerr").innerHTML="-";
+        numresult=true;
+        numerr.style.color="white";
+    }
+    if(date.value=="")
+    {
+        document.getElementById("dateerr").innerHTML="Invalid date";
+        console.log("date error");
+        dateresult=false;
+    }
+    else{
+        document.getElementById("dateerr").innerHTML="-";
+        dateresult=true;
+        dateerr.style.color="white";
+    }
+    if(nameresult==true&&numresult==true&&dateresult==true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
